3197c67d93d55aea12dcc6d0c5801bfea314b29a
snehalbargaje291 <snehal.bargaje291@gmail.com>
2024-09-26 12:28:25 +0530

    Table dropdown integrated

--- taxsarthi-admin/components/Table.tsx
+++ taxsarthi-admin/components/Table.tsx
@@ -1,12 +1,15 @@
-"use client";
-import React from "react";
-import {
-  DataGrid,
-  GridColDef,
-  GridRenderCellParams,
-} from "@mui/x-data-grid";
-import AssignDropdown from "./AssignDropdown";
+import React, { useState, useEffect } from "react";
+import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 import Actions from "./Actions";
+import {
+  Select,
+  MenuItem,
+  CircularProgress,
+  SelectChangeEvent,
+  Skeleton,
+} from "@mui/material";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { db } from "@/lib/firebase";
 
 type UserTask = {
   id: string;
@@ -39,8 +42,195 @@ const statusOptions = [
   "ITR Pending",
   "Refund Processed",
 ];
-const assignPlaceholder = "Select assignee";
-const statusPlaceholder = "Select status";
+
+// StatusCell Component
+const StatusCell: React.FC<GridRenderCellParams> = (params) => {
+  const defaultStatus = params.row.lastStatus || "";
+  const [selectedStatus, setSelectedStatus] = useState<string>(defaultStatus);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const currentYear = process.env.NEXT_PUBLIC_CURRENT_YEAR;
+        if (!currentYear) {
+          throw new Error("Current year is not defined");
+        }
+        const docRef = doc(db, currentYear.toString(), params.row.pan);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          const data = docSnap.data();
+          if (data.lastStatus) {
+            setSelectedStatus(data.lastStatus);
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [params.row.pan]);
+
+  const handleStatusChange = async (event: SelectChangeEvent<any>) => {
+    const newStatus = event.target.value;
+    const currentYear = process.env.NEXT_PUBLIC_CURRENT_YEAR;
+    if (!currentYear) {
+      throw new Error("Current year is not defined");
+    }
+    const docRef = doc(db, currentYear.toString(), params.row.pan);
+
+    try {
+      const statusObject = statusOptions.reduce(
+        (obj: { [key: string]: boolean }, status, index) => {
+          obj[status] = index <= statusOptions.indexOf(newStatus as string);
+          return obj;
+        },
+        {}
+      );
+
+      await updateDoc(docRef, { status: statusObject, lastStatus: newStatus });
+      setSelectedStatus(newStatus);
+      console.log("Status updated successfully");
+    } catch (error) {
+      console.error("Error updating status:", error);
+    }
+  };
+
+  if (loading) {
+    return (
+      <div className="mt-2">
+        <Skeleton variant="text" width={150} height={24} />
+      </div>
+    );
+  }
+
+  return (
+    <Select
+      sx={{
+        boxShadow: "none",
+        ".MuiOutlinedInput-notchedOutline": { border: 0 },
+        "&.MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline":
+                  {
+                    border: 0,
+                  },
+                "&.MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+                  {
+                    border: 0,
+                  },
+      }}
+      value={selectedStatus}
+      onChange={handleStatusChange}
+      className="border-0 w-[220px]"
+    >
+      {statusOptions.map((option) => (
+        <MenuItem key={option} value={option}>
+          <p className="text-sm">{option}</p>
+        </MenuItem>
+      ))}
+    </Select>
+  );
+};
+
+// AssignCell Component
+const AssignCell: React.FC<GridRenderCellParams> = (params) => {
+  const defaultAssign = params.row.assign || "";
+  const [selectedAssign, setSelectedAssign] = useState<string>(defaultAssign);
+  const [loading, setLoading] = useState(true);
+
+  // Parse the environment variable into an array
+  const assignOptions = process.env.NEXT_PUBLIC_OPS_EMAILS
+    ? process.env.NEXT_PUBLIC_OPS_EMAILS.split(",")
+    : [];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const currentYear = process.env.NEXT_PUBLIC_CURRENT_YEAR; // Make sure to update this if needed
+        if (!currentYear) {
+          throw new Error("Current year is not defined");
+        }
+        const docRef = doc(db, currentYear.toString(), params.row.pan);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          const data = docSnap.data();
+          if (data.assign) {
+            setSelectedAssign(data.assign);
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [params.row.pan]);
+
+  const handleAssignChange = async (event: SelectChangeEvent<any>) => {
+    const newAssign = event.target.value;
+    const currentYear = process.env.NEXT_PUBLIC_CURRENT_YEAR; // Update this as needed
+    if (!currentYear) {
+      throw new Error("Current year is not defined");
+    }
+    const docRef = doc(db, currentYear.toString(), params.row.pan);
+
+    try {
+      await updateDoc(docRef, { assign: newAssign });
+      setSelectedAssign(newAssign);
+      console.log("Assignment updated successfully");
+    } catch (error) {
+      console.error("Error updating assignment:", error);
+    }
+  };
+
+  if (loading) {
+    return (
+      <div className="mt-2">
+        <Skeleton variant="text" width={150} height={24} />
+      </div>
+    );
+  }
+
+  const formatDisplayName = (email: string) => {
+    return email
+      .split("+")[0] // Get the part before the +
+      .split("@")[0] // Get the part before the @
+      .split(".")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(" ");
+  };
+
+  return (
+    <Select
+      sx={{
+        boxShadow: "none",
+        ".MuiOutlinedInput-notchedOutline": { border: 0 },
+        "&.MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline":
+                  {
+                    border: 0,
+                  },
+                "&.MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+                  {
+                    border: 0,
+                  },
+      }}
+      value={selectedAssign}
+      onChange={handleAssignChange}
+      className="w-[140px] bg-transparent text-sm"
+    >
+      {assignOptions.map((option) => {
+        const displayName = formatDisplayName(option);
+        return (
+          <MenuItem key={option} value={option}>
+            <p className="text-sm">{displayName}</p>
+          </MenuItem>
+        );
+      })}
+    </Select>
+  );
+};
 
 const columns: GridColDef[] = [
   { field: "srNo", headerName: "Sr.", width: 80 },
@@ -62,53 +252,20 @@ const columns: GridColDef[] = [
     field: "assign",
     headerName: "Assign",
     width: 150,
-    sortable: false,
-    editable: true,
-    renderCell: (params: GridRenderCellParams) => (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100%",
-        }}
-      >
-        <AssignDropdown
-          value={params.value || ""}
-          api={params.api}
-          id={params.id}
-          field={params.field}
-          options={assignOptions}
-          placeholder={assignPlaceholder}
-        />
-      </div>
-    ),
+    renderCell: (params) => <AssignCell {...params} />,
   },
   {
     field: "lastStatus",
     headerName: "Status",
-    width: 150,
-    editable: true,
-    sortable: false,
-    renderCell: (params: GridRenderCellParams) => (
-      <div style={{ display: "flex", alignItems: "center", height: "100%" }}>
-        <AssignDropdown
-          value={params.value || ""}
-          api={params.api}
-          id={params.id}
-          field={params.field}
-          options={statusOptions}
-          placeholder={statusPlaceholder}
-        />
-      </div>
-    ),
+    width: 230,
+    renderCell: (params) => <StatusCell {...params} />,
   },
   {
     sortable: false,
     field: "actions",
     headerName: "Actions",
     width: 200,
-    renderCell: (params) => <Actions />,
+    renderCell: (params) => <Actions userData={params.row} />,
   },
 ];
 
@@ -116,14 +273,14 @@ const Table: React.FC<Props> = ({ rows, loading }) => {
   return (
     <div>
       <DataGrid
-      autoHeight
-      loading={loading}
-      slotProps={{
-        loadingOverlay: {
-          variant: 'skeleton',
-          noRowsVariant: 'skeleton',
-        },
-      }}
+        autoHeight
+        loading={loading}
+        slotProps={{
+          loadingOverlay: {
+            variant: "skeleton",
+            noRowsVariant: "skeleton",
+          },
+        }}
         rows={rows}
         columns={columns}
         processRowUpdate={(newRow) => newRow}
@@ -134,6 +291,8 @@ const Table: React.FC<Props> = ({ rows, loading }) => {
           },
         }}
         disableColumnMenu
+        pageSizeOptions={[10, 25, 50]} // Adding options for page size
+        getRowId={(row) => row.id} // Ensure unique row identifiers
       />
     </div>
   );
